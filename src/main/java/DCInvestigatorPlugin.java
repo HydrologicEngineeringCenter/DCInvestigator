ad08f513e3412ed4825bf2afaecf78e071d5fa77
Brennan Beam <BrennanBeam@gmail.com>
2021-04-09 12:02:06 -0700

    Convert to Gradle Build

--- src/main/java/DCInvestigatorPlugin.java
+++ src/main/java/DCInvestigatorPlugin.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DCInvestigator;
 import com.rma.client.Browser;
 import com.rma.client.BrowserAction;
 import com.rma.io.DssFileManagerImpl;
