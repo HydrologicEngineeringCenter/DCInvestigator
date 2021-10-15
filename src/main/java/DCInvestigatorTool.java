da261d2426060f2ee0b15ef07d3ccf9812c7e950
HEC <brennanbeam@gmail.com>
2021-10-14 22:57:54 -0700

    adding report writing to tool

--- src/main/java/DCInvestigatorTool.java
+++ src/main/java/DCInvestigatorTool.java
@@ -2,6 +2,9 @@ import hec.heclib.dss.DSSPathname;
 import hec.heclib.dss.HecDataManager;
 import hec.heclib.dss.HecPairedData;
 import hec.io.PairedDataContainer;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,10 +12,10 @@ import java.util.Vector;
 
 
 public class DCInvestigatorTool {
+
     private String _dssFilePath;
     private int _lifecyclesPerReal;
     private int _eventsPerLifecycle;
-
     Set<FailedEvent> _failedEvents = new HashSet<>();
 
     public DCInvestigatorTool(String _dssFilePath, int _lifecyclesPerReal, int _eventsPerLifecycle) {
@@ -22,13 +25,61 @@ public class DCInvestigatorTool {
         Investigate();
     }
 
-    public Set<FailedEvent> get_failedEvents() {
+    public Set<FailedEvent> GetFailedEvents() {
         return _failedEvents;
     }
-
+    public Set<Integer> GetBadLifecycles(){
+        Set<Integer> badLifecycles = new HashSet<>();
+        for(FailedEvent fail: _failedEvents){
+            badLifecycles.add(fail.getLifecycle());
+        }
+        return badLifecycles;
+    }
+    public Set<Integer> GetBadEventsPerLifecycle(int lifecycle){
+        Set<Integer> badEvents = new HashSet<>();
+        for(FailedEvent fail: _failedEvents){
+            if( fail.getLifecycle() == lifecycle){
+                badEvents.add(fail.getEvent());
+            }
+        }
+        return badEvents;
+    }
+    public int GetNumberOfRealizations(){
+        Set<Integer> reals = new HashSet<>();
+        Vector<String> OutputVariablePathnames = GetOutputVariablePathnames();
+        for(String pathname: OutputVariablePathnames){
+            DSSPathname dssPathname = new DSSPathname(pathname);
+            String collectionNum = dssPathname.getCollectionSequence();
+            reals.add(Integer.valueOf(collectionNum));
+        }
+        return reals.size();
+    }
+    public void WriteReport(String outputFilePath) {
+        int totalMissingEvents = 0;
+        try {
+            FileWriter myWriter = new FileWriter(outputFilePath);
+            //write all lifecycles with any missing data
+            myWriter.write("DCInvestigator Report for: " + _dssFilePath + "\n");
+            myWriter.write("This WAT Results DSS file contains " + GetNumberOfRealizations() + " realizations" + "\n\n");
+            myWriter.write("The following lifecycles have missing data: " + "\n");
+            myWriter.write(GetBadLifecycles() + "\n\n");
+            myWriter.write("The following list missing events for each lifecycle: " + "\n");
+            //for each of those lifecycles, write which data is missing
+            for (Integer eachLifecycle : GetBadLifecycles()) {
+                Set<Integer> failedEvents = GetBadEventsPerLifecycle(eachLifecycle);
+                totalMissingEvents += failedEvents.size();
+                myWriter.write((eachLifecycle) + ": " + failedEvents + "\n");
+            }
+            myWriter.write("Total Events Failed = " + totalMissingEvents + "\n\n");
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
     private void Investigate() {
 
-        Vector<String> pathnames = GetOutputVariablePathnames(_dssFilePath);
+        Vector<String> pathnames = GetOutputVariablePathnames();
         int failedRealization = 0;
         int failedLifecycle = 0;
         int failedEvent = 0;
@@ -73,169 +124,14 @@ public class DCInvestigatorTool {
             }
         }
     }
-    public Set<Integer> GrabBadLifecycles(){
-        Set<Integer> badLifecycles = new HashSet<>();
-        for(FailedEvent fail: _failedEvents){
-            badLifecycles.add(fail.getLifecycle());
-        }
-        return badLifecycles;
-    }
-    public Set<Integer> GrabBadEvents(int lifecycle){
-        Set<Integer> badEvents = new HashSet<>();
-        for(FailedEvent fail: _failedEvents){
-            if( fail.getLifecycle() == lifecycle){
-                badEvents.add(fail.getEvent());
-            }
-        }
-        return badEvents;
-    }
-
-    public static Vector<String> GetOutputVariablePathnames(String dssFileName) {
+    private Vector<String> GetOutputVariablePathnames() {
         String dssPartDvalue = "Output Variable";
         String pathWithWildChars = "/*/*/*/*" + dssPartDvalue + "*/*/*/";
         HecDataManager dssManager = new HecDataManager();
-        dssManager.setDSSFileName(dssFileName);
+        dssManager.setDSSFileName(_dssFilePath);
         String[] pathnames = dssManager.getCatalog(false, pathWithWildChars);
         HecDataManager.closeAllFiles();
         Vector<String> vector = new Vector<>(Arrays.asList(pathnames));
         return vector;
     }
-
-    public static int GetNumberOfRealizations(String filePath){
-        Set<Integer> reals = new HashSet<>();
-        Vector<String> OutputVariablePathnames = GetOutputVariablePathnames(filePath);
-
-        for(String pathname: OutputVariablePathnames){
-            DSSPathname dssPathname = new DSSPathname(pathname);
-            String collectionNum = dssPathname.getCollectionSequence();
-            reals.add(Integer.valueOf(collectionNum));
-        }
-        return reals.size();
-    }
-
-    public static Vector<String> GetIncompleteCollections(String filePath, int lifecyclesPerReal, int numMissingEvents){
-        Vector<String> incompleteRealizations = new Vector<>();
-        Vector<String> OutputVariablePathnames = GetOutputVariablePathnames(filePath);
-        PairedDataContainer mypdc = new PairedDataContainer();
-        HecPairedData pairedData = new HecPairedData();
-        pairedData.setDSSFileName(filePath);
-
-
-        for(String pathname: OutputVariablePathnames){
-            Boolean realizationIsIncomplete = false;
-            mypdc.setFullName(pathname);
-            pairedData.read(mypdc);
-            if(mypdc.getNumberCurves() != lifecyclesPerReal){
-                incompleteRealizations.add(pathname);
-                continue;
-            }
-            double[][] yOrds = mypdc.getYOridnates();
-            for(double[] lifecycle: yOrds){
-                int failedEventCount = 0;
-                for(double event: lifecycle){
-                    if(event < 0 ){
-                        failedEventCount++;
-                    }
-                    if (failedEventCount > numMissingEvents){
-                        incompleteRealizations.add(pathname);
-                        realizationIsIncomplete = true;
-                        break;
-                    }
-                }
-                if(realizationIsIncomplete){
-                    break;
-                }
-            }
-        }
-        return incompleteRealizations;
-    }
-
-
-
-    @Deprecated
-    public static Set<Integer> IdentifyBadLifecycles(String filePath, int lifecyclesPerRealization){
-        PairedDataContainer pdc = new PairedDataContainer();
-        HecPairedData pairedData = new HecPairedData();
-        pairedData.setDSSFileName(filePath);
-        Vector<String> pathnames = GetOutputVariablePathnames(filePath);
-        Set<Integer> errorList = new HashSet<>();
-        for(String pathname: pathnames){
-            pdc.setFullName(pathname);
-            pairedData.read(pdc);
-            DSSPathname tmp = new DSSPathname(pdc.fullName);
-            String collectionNum = tmp.getCollectionSequence();
-            int real = Integer.valueOf(collectionNum);
-
-            if(pdc.getNumberCurves() != lifecyclesPerRealization){
-                for(int lifecycle = pdc.getNumberCurves()+1; lifecycle <= lifecyclesPerRealization; lifecycle++){
-                    errorList.add(real*lifecyclesPerRealization+lifecycle);
-                }
-            }
-            double[][] yOrds = pdc.yOrdinates;
-            int lifecycleCount = 0;
-            for(double[] lifecycle: yOrds){
-                lifecycleCount++;
-                Boolean lifecycleHasErrors = false;
-                int eventCount = 0;
-                for(double event: lifecycle){
-                    eventCount++;
-                    if(event < 0){
-                        lifecycleHasErrors = true;
-                        break;
-                    }
-                }
-                if(lifecycleHasErrors){
-                    errorList.add(real*lifecyclesPerRealization+lifecycleCount);
-                }
-            }
-        }
-        return errorList;
-    }
-    @Deprecated
-    public static Set<Integer> IdentifyBadEvents(String filePath, int lifecycle, int lifecyclesPerRealization ){
-        PairedDataContainer pdc = new PairedDataContainer();
-        HecPairedData pairedData = new HecPairedData();
-        pairedData.setDSSFileName(filePath);
-
-        Set<Integer> badEvents = new HashSet<>();
-
-        int collectionMemberNumber;
-        int lifecycleIndex;
-        if(lifecycle%lifecyclesPerRealization==0) {
-            collectionMemberNumber = lifecycle / lifecyclesPerRealization - 1;
-            lifecycleIndex = 19;
-        }
-        else{
-            collectionMemberNumber = lifecycle/lifecyclesPerRealization;
-            lifecycleIndex = lifecycle%lifecyclesPerRealization-1;
-        }
-
-        Vector<String> pathnames = GetOutputVariablePathnames(filePath);
-        for(String pathname: pathnames){
-            DSSPathname name = new DSSPathname(pathname);
-            if(Integer.valueOf(name.getCollectionSequence()) != collectionMemberNumber){
-                continue;
-            }
-            pdc.setFullName(pathname);
-            pairedData.read(pdc);
-            double[][]yOrds = pdc.getYOridnates();
-            if(pdc.getNumberCurves() <= lifecycleIndex){
-                for(int i = 1; i<= pdc.getNumberOrdinates(); i++ ){
-                    badEvents.add(i);
-                }
-            }
-            else{
-                for(int i = 0; i < yOrds[lifecycleIndex].length; i++){
-                    if(yOrds[lifecycleIndex][i] < 0){
-                        badEvents.add(i+1);
-                    }
-                }
-            }
-
-        }
-        return badEvents;
-
-    }
-
-
 }
