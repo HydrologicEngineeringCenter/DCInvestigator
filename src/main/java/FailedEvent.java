15b4b1d9238d4caa854fca5033d17fa0520afb07
HEC <brennanbeam@gmail.com>
2021-10-14 22:01:02 -0700

    Updates to failed event to correctly work in Java hash sets

--- src/main/java/FailedEvent.java
+++ src/main/java/FailedEvent.java
@@ -3,6 +3,12 @@ public class FailedEvent {
     int Lifecycle;
     int Event;
 
+    public FailedEvent(int realization, int lifecycle, int event) {
+        Realization = realization;
+        Lifecycle = lifecycle;
+        Event = event;
+    }
+
     public int getRealization() {
         return Realization;
     }
@@ -15,9 +21,25 @@ public class FailedEvent {
         return Event;
     }
 
-    public FailedEvent(int realization, int lifecycle, int event) {
-        Realization = realization;
-        Lifecycle = lifecycle;
-        Event = event;
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if(getClass() != obj.getClass()) return false;
+        FailedEvent other = (FailedEvent) obj;
+        if(Realization != other.getRealization()) return false;
+        if(Lifecycle != other.getLifecycle()) return false;
+        if(Event != other.getEvent()) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Realization;
+        result = prime * result + Lifecycle;
+        result = prime * result + Event;
+        return result;
     }
 }
